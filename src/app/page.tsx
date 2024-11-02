66cad900a09c11aeaade3593bd4faa8d023ed2ba
whispnode <whispnode@gmail.com>
2024-11-02 22:53:44 +0300

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
-    return <div>hello</div>;
+    return (
+        <div className="w-full h-screen flex items-center justify-center flex-col gap-2">
+            <p className="text-center text-xl">Hello there!! 👋</p>
+            <p className="text-center">Welcome to Sudoku Game</p>
+            <Link
+                href="/game"
+                type="button"
+                className="px-5 py-3 text-sm bg-zinc-900 text-zinc-50 mt-5"
+            >
+                Start Game
+            </Link>
+        </div>
+    );
 }
